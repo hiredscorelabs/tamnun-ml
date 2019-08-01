9c141eebe246a889573e16d4050033b5138ef66b
Dima Shulga <shudima@gmail.com>
2019-08-01 10:45:59 +0300

    add description

--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='tamnun',
+    version="0.1.0",
+    description="An easy to use open-source library for advanced Deep Learning and Natural Language Processing",
+    long_description=open("README.md", "r", encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    keywords='Deep Learning Natural Language Processing NLP Machine Learning Transfer Learning',
+    license='Apache',
     packages=find_packages(exclude=['test*']),
     install_requires=[
         'numpy==1.15.4',
